dc563eeb7b7f08d01e82b90e25f247ff6d170f9b
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2021-02-23 19:31:38 +0200

    Делает верстку секции "Наши услуги это эффективные решения для Вашего бизнеса"

--- src/js/svg-sprite.js
+++ src/js/svg-sprite.js
@@ -1,3 +1,4 @@
 import '../img/svg-sprite/icon-angle-down-light.svg'
 import '../img/svg-sprite/icon-telegram.svg'
 import '../img/svg-sprite/icon-whatsapp.svg'
+import '../img/svg-sprite/icon-arrow-right.svg'
