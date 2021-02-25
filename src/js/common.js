6f5674febe45954fb92a17f1ab262a1cec7a9a53
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2021-02-25 22:36:03 +0200

    Делает фиксированную шапку и валидацию форм

--- src/js/common.js
+++ src/js/common.js
@@ -1,11 +1,69 @@
 import '@fancyapps/fancybox/dist/jquery.fancybox.min';
+import 'waypoints/lib/noframework.waypoints.min';
+import 'jquery-validation';
 import 'focus-visible';
-import Inputmask from "inputmask";
 import './google-maps';
 import './svg-sprite';
+import Inputmask from 'inputmask';
 
 $(document).ready(function () {
     'use strict';
+
+    // START STICKY HEADER
+    ////////////////////////////////////////////////////////////////////////////
+    let header = document.querySelector('.header');
+    let headerWr = document.querySelector('.header__wr');
+    let waypointOffset = 0;
+
+    // start sticky header
+    let waypoint = new Waypoint({
+        element: header,
+        handler: function (direction) {
+            if (direction === 'down') {
+                header.style.height = `${headerWr.offsetHeight}px`;
+                headerWr.classList.add('header__wr--start-sticky');
+                headerWr.classList.remove('header__wr--end-sticky');
+            }
+        },
+        offset: function() {
+            return -(this.element.clientHeight + waypointOffset);
+        }
+    });
+
+    // end sticky header
+    let waypointMainNavTop = new Waypoint({
+        element: header,
+        handler: function (direction) {
+            if (direction === 'up') {
+                header.style.height = 'auto';
+                headerWr.classList.remove('header__wr--start-sticky');
+            }
+        },
+        offset: -1,
+    });
+
+    // show/hide header when scrolling
+    let scrollPos = 0;
+
+    window.addEventListener('scroll', () => {
+        if (document.body.getBoundingClientRect().top > scrollPos) {
+            // SCROLL UP
+            if (headerWr.classList.contains('header__wr--start-sticky')) {
+                headerWr.classList.add('header__wr--show');
+            }
+            headerWr.classList.remove('header__wr--hide');
+        } else {
+            // SCROLL DOWN
+            if (headerWr.classList.contains('header__wr--start-sticky')) {
+                headerWr.classList.add('header__wr--hide');
+            }
+            headerWr.classList.remove('header__wr--show');
+        }
+
+        scrollPos = document.body.getBoundingClientRect().top;
+    }, {passive: true});
+    // END STICKY HEADER
+    ////////////////////////////////////////////////////////////////////////////
 
     // START DETERMINING FIELD STATUS
     ////////////////////////////////////////////////////////////////////////////
@@ -43,4 +101,67 @@ $(document).ready(function () {
     // END INIT INPUT MASK
     ////////////////////////////////////////////////////////////////////////////
 
+    // START FORM VALIDATION
+    ////////////////////////////////////////////////////////////////////////////
+    let forms = document.querySelectorAll('form');
+
+    forms.forEach( form => {
+
+        // add a custom validation method
+        $.validator.addMethod(
+            'regex',
+            function(value, element, regexp) {
+                let re = new RegExp(regexp);
+                return this.optional(element) || re.test(value);
+            },
+            'Это поле необходимо заполнить.'
+        );
+
+        let validator = $(form).validate({
+            errorPlacement: function(label, elem) {
+                let parent = elem.parent();
+                parent.append(label);
+            },
+            rules: {
+                'name': 'required',
+                'phone': {
+                    required: true,
+                    regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$',
+                },
+                'email': {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+
+            }
+        });
+    });
+
+    /*
+    * Translated default messages for the jQuery validation plugin.
+    * Locale: RU (Russian; русский язык)
+    */
+    $.extend( $.validator.messages, {
+        required: "Это поле необходимо заполнить.",
+        remote: "Пожалуйста, введите правильное значение.",
+        email: "Пожалуйста, введите корректный адрес электронной почты.",
+        url: "Пожалуйста, введите корректный URL.",
+        date: "Пожалуйста, введите корректную дату.",
+        dateISO: "Пожалуйста, введите корректную дату в формате ISO.",
+        number: "Пожалуйста, введите число.",
+        digits: "Пожалуйста, вводите только цифры.",
+        creditcard: "Пожалуйста, введите правильный номер кредитной карты.",
+        equalTo: "Пожалуйста, введите такое же значение ещё раз.",
+        extension: "Пожалуйста, выберите файл с правильным расширением.",
+        maxlength: $.validator.format( "Пожалуйста, введите не больше {0} символов." ),
+        minlength: $.validator.format( "Пожалуйста, введите не меньше {0} символов." ),
+        rangelength: $.validator.format( "Пожалуйста, введите значение длиной от {0} до {1} символов." ),
+        range: $.validator.format( "Пожалуйста, введите число от {0} до {1}." ),
+        max: $.validator.format( "Пожалуйста, введите число, меньшее или равное {0}." ),
+        min: $.validator.format( "Пожалуйста, введите число, большее или равное {0}." )
+    } );
+    // END FORM VALIDATION
+    ////////////////////////////////////////////////////////////////////////////
 }); // end ready
